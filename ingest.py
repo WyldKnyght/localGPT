6644c080ac52c0cf90bdbe1ec9032e48a60ad18b
Sourcery AI <>
2024-01-06 17:09:04 +0000

    'Refactored by Sourcery'

--- ingest.py
+++ ingest.py
@@ -19,82 +19,80 @@ from constants import (
 )
 
 def file_log(logentry):
-   file1 = open("file_ingest.log","a")
-   file1.write(logentry + "\n")
-   file1.close()
+   with open("file_ingest.log","a") as file1:
+      file1.write(logentry + "\n")
    print(logentry + "\n")
 
 def load_single_document(file_path: str) -> Document:
     # Loads a single document from a file path
-    try:
-       file_extension = os.path.splitext(file_path)[1]
-       loader_class = DOCUMENT_MAP.get(file_extension)
-       if loader_class:
-           file_log(file_path + ' loaded.')
-           loader = loader_class(file_path)
-       else:
-           file_log(file_path + ' document type is undefined.')
-           raise ValueError("Document type is undefined")
-       return loader.load()[0]
-    except Exception as ex:
-       file_log('%s loading error: \n%s' % (file_path, ex))
-       return None 
+   try:
+      file_extension = os.path.splitext(file_path)[1]
+      if loader_class := DOCUMENT_MAP.get(file_extension):
+         file_log(f'{file_path} loaded.')
+         loader = loader_class(file_path)
+      else:
+         file_log(f'{file_path} document type is undefined.')
+         raise ValueError("Document type is undefined")
+      return loader.load()[0]
+   except Exception as ex:
+      file_log('%s loading error: \n%s' % (file_path, ex))
+      return None 
 
 def load_document_batch(filepaths):
-    logging.info("Loading document batch")
+   logging.info("Loading document batch")
     # create a thread pool
-    with ThreadPoolExecutor(len(filepaths)) as exe:
-        # load files
-        futures = [exe.submit(load_single_document, name) for name in filepaths]
+   with ThreadPoolExecutor(len(filepaths)) as exe:
+      # load files
+      futures = [exe.submit(load_single_document, name) for name in filepaths]
         # collect data
-        if futures is None:
-           file_log(name + ' failed to submit')
-           return None
-        else:
-           data_list = [future.result() for future in futures]
-           # return data and file paths
-           return (data_list, filepaths)
+      if futures is None:
+         file_log(f'{name} failed to submit')
+         return None
+      else:
+         data_list = [future.result() for future in futures]
+         # return data and file paths
+         return (data_list, filepaths)
 
 
 def load_documents(source_dir: str) -> list[Document]:
-    # Loads all documents from the source documents directory, including nested folders
-    paths = []
-    for root, _, files in os.walk(source_dir):
-        for file_name in files:
-            print('Importing: ' + file_name)
-            file_extension = os.path.splitext(file_name)[1]
-            source_file_path = os.path.join(root, file_name)
-            if file_extension in DOCUMENT_MAP.keys():
-                paths.append(source_file_path)
+   # Loads all documents from the source documents directory, including nested folders
+   paths = []
+   for root, _, files in os.walk(source_dir):
+      for file_name in files:
+         print(f'Importing: {file_name}')
+         file_extension = os.path.splitext(file_name)[1]
+         source_file_path = os.path.join(root, file_name)
+         if file_extension in DOCUMENT_MAP.keys():
+             paths.append(source_file_path)
 
-    # Have at least one worker and at most INGEST_THREADS workers
-    n_workers = min(INGEST_THREADS, max(len(paths), 1))
-    chunksize = round(len(paths) / n_workers)
-    docs = []
-    with ProcessPoolExecutor(n_workers) as executor:
-        futures = []
+   # Have at least one worker and at most INGEST_THREADS workers
+   n_workers = min(INGEST_THREADS, max(len(paths), 1))
+   chunksize = round(len(paths) / n_workers)
+   docs = []
+   with ProcessPoolExecutor(n_workers) as executor:
+      futures = []
         # split the load operations into chunks
-        for i in range(0, len(paths), chunksize):
-            # select a chunk of filenames
-            filepaths = paths[i : (i + chunksize)]
+      for i in range(0, len(paths), chunksize):
+         # select a chunk of filenames
+         filepaths = paths[i : (i + chunksize)]
             # submit the task
-            try:
-               future = executor.submit(load_document_batch, filepaths)
-            except Exception as ex:
-               file_log('executor task failed: %s' % (ex))
-               future = None
-            if future is not None:
-               futures.append(future)
+         try:
+            future = executor.submit(load_document_batch, filepaths)
+         except Exception as ex:
+            file_log(f'executor task failed: {ex}')
+            future = None
+         if future is not None:
+            futures.append(future)
         # process all results
-        for future in as_completed(futures):
+      for future in as_completed(futures):
             # open the file and load the data
-            try:
-                contents, _ = future.result()
-                docs.extend(contents)
-            except Exception as ex:
-                file_log('Exception: %s' % (ex))
-                
-    return docs
+         try:
+            contents, _ = future.result()
+            docs.extend(contents)
+         except Exception as ex:
+            file_log(f'Exception: {ex}')
+
+   return docs
 
 
 def split_documents(documents: list[Document]) -> tuple[list[Document], list[Document]]:
